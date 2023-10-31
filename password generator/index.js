23a8c2ef1b5385a7b3a9b151de9f479afd9b0822
Kyle Piacenta <124377155+kpiacenta93@users.noreply.github.com>
2023-10-31 19:49:21 -0400

    Add files via upload

---
+++ password generator/index.js
@@ -0,0 +1,43 @@
+const passwordBox = document.getElementById("password");
+
+const length = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const lowerCase = "abcefghijklnopqrstuvwxyz"
+const number = "0123456789"
+const symbols = "!@#$%^&*?"
+
+const allChars = upperCase + lowerCase + number + symbols
+
+function createPass(){
+    let password = ""
+
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)]
+    password += number[Math.floor(Math.random() * symbols.length)]
+    
+
+    while (length > password.length){
+        password += allChars[Math.floor(Math.random() * allChars.length)]
+}
+
+passwordBox.value = password;
+    }
+
+
+function copyText(){
+    let copyOfText = document.getElementById("password");
+
+    copyOfText.select();
+    
+    navigator.clipboard.writeText(copyOfText.value);
+
+    alert("copied the text: " + copyOfText.value);
+
+}
+
+
+
+
+
